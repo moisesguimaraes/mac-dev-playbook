2430fa9beb494fbfa91a2d0bfed4e81c607f445b
Moisés Guimarães de Medeiros <guimaraes@pm.me>
2020-08-28 18:34:30 +0200

    prepare bootstrap for Fedora

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
+DNF_CMD=$(which dnf)
+
 set -e
 
-# Download and install Command Line Tools
-if [[ ! -x /usr/bin/gcc ]]; then
-    echo "Info   | Install   | xcode"
-    xcode-select --install
-fi
+# Mac OSX
+if [[ $(uname -s) = Darwin ]]; then
+    # Download and install Command Line Tools
+    if [[ ! -x /usr/bin/gcc ]]; then
+        echo "Info   | Installing | xcode"
+        xcode-select --install
+    fi
 
-# Download and install Homebrew
-if [[ ! -x /usr/local/bin/brew ]]; then
-    echo "Info   | Install   | homebrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+    # Download and install Homebrew
+    if [[ ! -x /usr/local/bin/brew ]]; then
+        echo "Info   | Installing | homebrew"
+        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
 
-# Download and install Ansible
-if [[ ! -x /usr/local/bin/ansible ]]; then
-    brew install ansible
+    # Download and install Ansible
+    if [[ ! -x /usr/local/bin/ansible ]]; then
+        echo "Info   | Installing | ansible"
+        brew install ansible
+    fi
+
+# Fedora / CentOS / RHEL
+elif [[ -x $DNF_CMD ]]; then
+    # Download and install Ansible
+    echo "Info   | Installing | ansible"
+    dnf install -y ansible
+
+# ¯\_(ツ)_/¯
+else
+    echo "error: can't install ansible"
+    exit 1;
 fi
 
 ansible-galaxy install -r requirements.yml
 
 echo -e "\nYour system is ready for provisioning!\n"
 echo -e "Run 'ansible-playbook main.yml -i inventory -K' to do so\n"
-
