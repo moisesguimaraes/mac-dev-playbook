1e4d8e6440218b89ea1693fa35d97f07fd311f1e
Moises Guimaraes de Medeiros <moguimar@redhat.com>
2020-08-28 18:49:00 +0200

    fix identation

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
+ANSIBLE_CMD=$(which ansible)
 DNF_CMD=$(which dnf)
 
 set -e
 
-# Mac OSX
-if [[ $(uname -s) = Darwin ]]; then
+# Ensure Ansible is installed
+if [[ ! -x $ANSIBLE_CMD ]]; then
+
+  # Mac OSX
+  if [[ $(uname -s) = Darwin ]]; then
     # Download and install Command Line Tools
     if [[ ! -x /usr/bin/gcc ]]; then
-        echo "Info   | Installing | xcode"
-        xcode-select --install
+      echo "Info   | Installing | xcode"
+      xcode-select --install
     fi
 
     # Download and install Homebrew
     if [[ ! -x /usr/local/bin/brew ]]; then
-        echo "Info   | Installing | homebrew"
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+      echo "Info   | Installing | homebrew"
+      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     fi
 
     # Download and install Ansible
     if [[ ! -x /usr/local/bin/ansible ]]; then
-        echo "Info   | Installing | ansible"
-        brew install ansible
+      echo "Info   | Installing | ansible"
+      brew install ansible
     fi
 
-# Fedora / CentOS / RHEL
-elif [[ -x $DNF_CMD ]]; then
+  # Fedora / CentOS / RHEL
+  elif [[ -x $DNF_CMD ]]; then
     # Download and install Ansible
     echo "Info   | Installing | ansible"
-    dnf install -y ansible
+    sudo dnf install -y ansible
 
-# ¯\_(ツ)_/¯
-else
+  # ¯\_(ツ)_/¯
+  else
     echo "error: can't install ansible"
     exit 1;
+  fi
 fi
 
 ansible-galaxy install -r requirements.yml
