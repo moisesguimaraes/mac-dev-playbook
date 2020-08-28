ddec2fe6768bd2e838a52177c2c4df5937ca1b53
Moisés Guimarães de Medeiros <guimaraes@pm.me>
2020-08-28 19:00:03 +0200

    clean bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,44 +1,41 @@
 #!/bin/bash
 
-ANSIBLE_CMD=$(which ansible)
-DNF_CMD=$(which dnf)
-
 set -e
 
 # Ensure Ansible is installed
-if [[ ! -x $ANSIBLE_CMD ]]; then
+if [[ ! -x $(which ansible) ]]; then
 
-  # Mac OSX
-  if [[ $(uname -s) = Darwin ]]; then
-    # Download and install Command Line Tools
-    if [[ ! -x /usr/bin/gcc ]]; then
-      echo "Info   | Installing | xcode"
-      xcode-select --install
+    # Mac OSX
+    if [[ $(uname -s) = Darwin ]]; then
+
+        # Download and install Command Line Tools
+        if [[ ! -x $(which gcc) ]]; then
+            echo "[INFO] Installing xcode"
+            xcode-select --install
+        fi
+
+        # Download and install Homebrew
+        if [[ ! -x $(which brew) ]]; then
+            echo "[INFO] Installing homebrew"
+            ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        fi
+
+        # Download and install Ansible
+        echo "[INFO] Installing ansible"
+        brew install ansible
+
+    # Fedora / CentOS / RHEL
+    elif [[ -x $(which dnf) ]]; then
+
+        # Download and install Ansible
+        echo "[INFO] Installing ansible"
+        sudo dnf install -y ansible
+
+    # ¯\_(ツ)_/¯
+    else
+        echo "[ERROR] can't install ansible"
+        exit 1;
     fi
-
-    # Download and install Homebrew
-    if [[ ! -x /usr/local/bin/brew ]]; then
-      echo "Info   | Installing | homebrew"
-      ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-
-    # Download and install Ansible
-    if [[ ! -x /usr/local/bin/ansible ]]; then
-      echo "Info   | Installing | ansible"
-      brew install ansible
-    fi
-
-  # Fedora / CentOS / RHEL
-  elif [[ -x $DNF_CMD ]]; then
-    # Download and install Ansible
-    echo "Info   | Installing | ansible"
-    sudo dnf install -y ansible
-
-  # ¯\_(ツ)_/¯
-  else
-    echo "error: can't install ansible"
-    exit 1;
-  fi
 fi
 
 ansible-galaxy install -r requirements.yml
